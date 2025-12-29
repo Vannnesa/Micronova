ba711258cbe6597414eb784a8e427272b7b812f2
VANNESA\Vannesa <Separate521@vannesa.top>
2025-12-29 22:48:14 +0800

    health manager

---
+++ src/client/java/top/vannesa/micronova/MicronovaDataGenerator.java
@@ -0,0 +1,11 @@
+package top.vannesa.micronova;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MicronovaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/shared/java/top/vannesa/micronova/shared/health/BodyPart.java
@@ -0,0 +1,11 @@
+package top.vannesa.micronova.shared.health;
+
+public enum BodyPart {
+    HEAD,
+    CHEST,
+    STOMACH,
+    LEFT_ARM,
+    RIGHT_ARM,
+    LEFT_LEG,
+    RIGHT_LEG
+}
