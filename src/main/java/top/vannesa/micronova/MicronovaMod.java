023cbf5855f42cc605528f81c98aec905ba4f461
VANNESA\Vannesa <Separate521@vannesa.top>
2025-12-30 23:05:06 +0800

    Edited MicronovaMod.java lesser bugs.

--- src/main/java/top/vannesa/micronova/MicronovaMod.java
+++ src/main/java/top/vannesa/micronova/MicronovaMod.java
@@ -1,24 +1,14 @@
 package top.vannesa.micronova;
 
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import top.vannesa.micronova.network.ModPackets;
+import top.vannesa.micronova.command.DebugHealthCommand;
 
 public class MicronovaMod implements ModInitializer {
-	public static final String MOD_ID = "micronova";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModPackets.registerServer();
+		DebugHealthCommand.register();
 	}
 }
